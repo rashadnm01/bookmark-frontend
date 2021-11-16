7f0bb33f03a40469ee0358d13dc96d5d60e98546
Max Said <maxsaid95@gmail.com>
2021-11-15 20:37:27 -0500

    moved from app.js to Bookmarks.js component

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,9 +1,70 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Index from "../pages/index";
+import Show from "../pages/show";
 
-export default function Bookmarks(props) {
+const Bookmarks = (props) => {
+  const [bookmarks, setBookmarks] = useState(null);
+  const URL = "https://bookmark-backennd.herokuapp.com/";
+
+  const getBookmarks = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setBookmarks(data);
+  };
+  const createBookmarks = async (bookmark) => {
+    await fetch(URL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookmark),
+    });
+    getBookmarks();
+  };
+  const updateBookmarks = async (bookmark, id) => {
+    await fetch(URL + id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bookmark),
+    });
+    getBookmarks();
+  };
+  const deleteBookmarks = async (id) => {
+    await fetch(URL + id, {
+      method: "delete",
+    });
+    getBookmarks();
+  };
+
+  useEffect(() => {
+    getBookmarks();
+  }, []);
+
   return (
-    <section>
-      <a href={props.link}>{props.website}</a>
-    </section>
+    <main>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Index bookmarks={bookmarks} createBookmarks={createBookmarks} />
+          }
+        />
+        <Route
+          path="/bookmarks/:id"
+          element={
+            <Show
+              bookmarks={bookmarks}
+              updateBookmarks={updateBookmarks}
+              deleteBookmarks={deleteBookmarks}
+            />
+          }
+        />
+      </Routes>
+    </main>
   );
-}
+};
+
+export default Bookmarks;
