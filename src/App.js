a8dcee43f69b6756cac82033dfd6b07c81b25c12
Max Said <maxsaid95@gmail.com>
2021-11-15 20:42:45 -0500

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,75 +1,11 @@
-import "./App.css";
-import React, { useState } from "react";
-
-// IMPORT COMPONENTS
 import Header from "./components/Header";
-import { Route, Routes } from "react-router-dom";
-// IMPORT PAGES
-import Index from "./pages/index";
-import Show from "./pages/show";
-import Bookmark from "./components/Bookmarks";
+import Bookmarks from ".components/Bookmarks";
 
 function App() {
-  const [bookmarks, setBookmarks] = useState(null);
-  const URL = "https://bookmark-backennd.herokuapp.com/";
-
-  const getBookmarks = async () => {
-    const response = await fetch(URL);
-    const data = await response.json();
-    setBookmarks(data);
-  };
-  const createBookmarks = async (bookmark) => {
-    await fetch(URL, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bookmark),
-    });
-    getBookmarks();
-  };
-  const updateBookmarks = async (bookmark, id) => {
-    await fetch(URL + id, {
-      method: "put",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bookmark),
-    });
-    getBookmarks();
-  };
-  const deleteBookmarks = async (id) => {
-    await fetch(URL + id, {
-      method: "delete",
-    });
-    getBookmarks();
-  };
-
-  useEffect(() => {
-    getBookmarks();
-  }, []);
-
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Index bookmarks={bookmarks} createBookmarks={createBookmarks} />
-          }
-        />
-        <Route
-          path="/bookmarks/:id"
-          element={
-            <Show
-              bookmarks={bookmarks}
-              updateBookmarks={updateBookmarks}
-              deleteBookmarks={deleteBookmarks}
-            />
-          }
-        />
-      </Routes>
+      <Bookmarks />
     </div>
   );
 }
